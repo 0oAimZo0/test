6f1793121b21b6760bdcaa12a8154415cae79953
Suwapat Kittinanon <suwapat.k@outlook.co.th>
2017-08-15 01:01:31 +0700

    fix path

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/aimtest", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("listening...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"message": "Hello"})
 	})
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
